8d6a4fc6f0ab2fd463cdbfcbc671afd814ac2b0b
walter siameto <siametowalter@gmail.com>
2020-08-30 02:35:30 +0300

    added some css

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-// Requires jQuery
-
 $(document).on('click','.js-menu_toggle.closed',function(e){
 	e.preventDefault(); $('.list_load, .list_item').stop();
 	$(this).removeClass('closed').addClass('opened');
@@ -34,6 +32,7 @@ $(document).on('click','.js-menu_toggle.opened',function(e){
 	$('.list_load').slideUp(300);
 });
 
+
 // readmore toggle
 
 window.onload = function() {
@@ -47,38 +46,39 @@ moreButton.click(function() {
 });
 }
 
+
 // disable right clicking
 
-window.onload = function() {
-  document.addEventListener("contextmenu", function(e){
-    e.preventDefault();
-  }, false);
-  document.addEventListener("keydown", function(e) {
-  //document.onkeydown = function(e) {
-    // "I" key
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
-      disabledEvent(e);
-    }
-    // "J" key
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
-      disabledEvent(e);
-    }
-    // "S" key + macOS
-     if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
-       disabledEvent(e);
-     }
-     // "U" key
-     if (e.ctrlKey && e.keyCode == 85) {
-       disabledEvent(e);
-     }
-     // "F12" key
-     if (event.keyCode == 123) {
-       disabledEvent(e);
-     } }, false); function disabledEvent(e){   if (e.stopPropagation){     e.stopPropagation();
-     } else if (window.event){
-       window.event.cancelBubble = true;
-     }
-     e.preventDefault();
-     return false;
-   }
- }
+// window.onload = function() {
+//   document.addEventListener("contextmenu", function(e){
+//     e.preventDefault();
+//   }, false);
+//   document.addEventListener("keydown", function(e) {
+//   //document.onkeydown = function(e) {
+//     // "I" key
+//     if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+//       disabledEvent(e);
+//     }
+//     // "J" key
+//     if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+//       disabledEvent(e);
+//     }
+//     // "S" key + macOS
+//      if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+//        disabledEvent(e);
+//      }
+//      // "U" key
+//      if (e.ctrlKey && e.keyCode == 85) {
+//        disabledEvent(e);
+//      }
+//      // "F12" key
+//      if (event.keyCode == 123) {
+//        disabledEvent(e);
+//      } }, false); function disabledEvent(e){   if (e.stopPropagation){     e.stopPropagation();
+//      } else if (window.event){
+//        window.event.cancelBubble = true;
+//      }
+//      e.preventDefault();
+//      return false;
+//    }
+//  }
